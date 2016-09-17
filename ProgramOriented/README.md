e0588e8a704ec93a3b6bfc692a6eb951a4ee1024
Abhijit Das <abhijit2294@hotmail.com>
2016-09-17 12:23:21 +0530

    Code has been optimised
    - first.c has been optimised
    - ProgramOriented/README.md added

---
+++ ProgramOriented/README.md
@@ -0,0 +1,27 @@
+#PROCEDURE TO RUN THE FILE
+
+<pre>
+$ cd CS702
+$ cd ProgramOriented
+$ gcc  gcd.c first.c  -o test
+$ ./test
+Enter Two numbers to calculate GCD:
+A = 212121
+B = 3232323
+
+(1) Recursive Algorithm or (2) Euclidean Algorithm 3) Brute-force Algorithm?
+Option = 1
+GCD VALUE : 9	Recursive CPU Time:0.000008
+
+(1) Recursive Algorithm or (2) Euclidean Algorithm 3) Brute-force Algorithm?
+Option = 2
+GCD VALUE : 9	Eucledean CPU Time:0.000009
+
+(1) Recursive Algorithm or (2) Euclidean Algorithm 3) Brute-force Algorithm?
+Option = 3
+GCD VALUE : 9	BruteForece CPU Time:0.035015
+
+(1) Recursive Algorithm or (2) Euclidean Algorithm 3) Brute-force Algorithm?
+Option = -1
+Invalid input! Using brute force algorithm! 
+</pre>
--- ProgramOriented/first.c
+++ ProgramOriented/first.c
@@ -19,20 +19,34 @@
  * Date   : 06/09/2016 
  */
 /* This program is used to calculate GCD using Euclidean algorithm[Iterative] & [Recursive] , Bruteforce Algorithm
- */ 
+ */
+
+/*
+ * Link the header file first using the following commad
+ * gcc  GCD.c CryptoFramework.c  -o test
+ * ./test**/
 #include<stdio.h>
 #include <time.h>
 #include "gcd.h"
+
+void mDisplay(int iValue, char* iOps);
+void display_CPU_Time(char* ch);
+char euc[30] = "Eucledean";
+char rec[30] = "Recursive";
+char bru[30] = "BruteForece";
+
+clock_t start,end;
+
 int main()
 {
 	long int a,b;
 	int c;
-	int ch;
-	clock_t start,end;
+	int ch=0;
+	
 	printf("Enter Two numbers to calculate GCD:\n");
-	printf("A=");
+	printf("A = ");
 	scanf("%ld",&a);
-	printf("B=");
+	printf("B = ");
 	scanf("%ld",&b);
 	
 	// Check for 0
@@ -45,31 +59,47 @@ int main()
 	if (a < 0) a = -a;
 	if (b < 0) b = -b;
 
-	printf("User (1) Recursive Algorithm or (2) Euclidean Algorithm 3) Brute-force Algorithm? ");
-	scanf("%d", &ch);
-	switch(ch){
-		case 1: 
-			printf("Recursive Approach\n");
-			start=clock();
-			c = gcd_recursive(a, b);
-			end=clock();
-		      break;
-		case 2:
-			printf("Euclidean Approach using Iteration\n");
-			start=clock(); 
-			c = gcd_euclidean(a, b);
-			end=clock();
-			break;
-		case 3:
-			printf("Euclidean Approach using Recursion\n");
-			start=clock();
-			c= gcd_bruteforce(a,b);
-			end=clock();
-			break;
-		default: 
-			printf("Invalid input! Using brute force algorithm!\n");
-	}
-	printf("GCD=%d\n",c);
-	printf("CPU Time:%lf\n",((double) (end - start)) / CLOCKS_PER_SEC);
-	return 0;
+    do {
+        printf("\n(1) Recursive Algorithm or (2) Euclidean Algorithm 3) Brute-force Algorithm?\n");
+        printf("Option = ");
+        scanf("%d", &ch);
+        
+        //Initializing the clock variable start
+        start = clock();
+        
+        switch (ch) {
+                
+            case 2:
+                //Euclidean Algorithm implementation
+                mDisplay(gcd_euclidean(a, b), euc);
+                break;
+                
+            case 1:
+                //Recursive Algorithm implementation
+                mDisplay(gcd_recursive(a, b), rec);
+                break;
+                
+            case 3:
+                //Brute Force Algorithm implementation
+                mDisplay(gcd_bruteforce(a,b),bru);
+                break;
+            default:
+            printf("Invalid input! Using brute force algorithm!\n");        }
+    } while (ch != -1);
+    
+		return 0;
+}
+
+
+//Display the output
+void mDisplay(int iValue, char* iOps) {
+    printf("GCD VALUE : %d\t", iValue);
+    display_CPU_Time(iOps);
+}
+
+//Returning CPU_TIME difference
+void display_CPU_Time(char* ch) {
+    end = clock();
+    printf("%s",ch);
+        printf(" CPU Time:%lf\n", ((double) (end - start)) / CLOCKS_PER_SEC);
 }
